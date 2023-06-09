a4b65b5899a6aa62994dd0314d0632d3c638c302
amird55 <amir.dgn@outlook.com>
2023-06-09 10:01:28 +0300

    init

--- routes/cat_R.js
+++ routes/cat_R.js
@@ -22,6 +22,19 @@ router.get('/List',async (req, res) => {
         data:cat_data
     });
 });
+router.get('/Edit',async (req, res) => {
+    let item_data=await catModel.findById(req.query.id);
+    res.render("catAdd", {pageTitle:"עריכת קטגוריה",
+        item:item_data
+    });
+});
+router.post('/Edit',async (req, res) => {
+    const modelData = {
+        name:req.body.name
+    };
+    let item_data=await catModel.findByIdAndUpdate(req.query.id,modelData);
+    res.redirect("/C/List");
+});
 
 
 
